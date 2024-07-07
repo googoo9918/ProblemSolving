c0d925bf9224560a7b08dd99efd3748d6eb1272e
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-07-07 23:55:26 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[][] arr) {
+        boolean check = true;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                if(arr[i][j] != arr[j][i]) check = false;
+            }
+        }
+        return check ? 1 : 0;
+    }
+}
