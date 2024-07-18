3108bef07551d04c73cee08e1bcde3c318469577
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-07-18 16:04:35 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 24520 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,43 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
   public void solution() throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    for(int i = 1; i <= N; i++) {
+      queue.offer(i);
+    }
+
     StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(st.nextToken());
-    int k = Integer.parseInt(st.nextToken());
-    boolean[] check = new boolean[n];
-    int[] ans = new int[n];
-    int cidx=0, aidx=0, cnt=0;
-    while(containsFalse(check)){
-      if(check[cidx]==false){
-        cnt++;
-        if(cnt==k){
-          cnt = 0;
-          ans[aidx++] = cidx;
-          check[cidx] = true;
-        }
+    sb.append('<');
+
+    while(queue.size() > 1) {
+      for(int i = 0; i < K-1; i++) {
+        queue.offer(queue.poll());
       }
-      if(cidx<n-1) cidx++;
-      else cidx -= n-1;
+      
+      sb.append(queue.poll()).append(", ");
     }
-    sb.append("<");
-    for(int i=0; i<n; i++){
-      if(i!=n-1) sb.append(ans[i]+1).append(", ");
-      else sb.append(ans[i]+1).append(">");
-    }
+
+    sb.append(queue.poll()).append('>');
+
     System.out.println(sb);
   }
   public static void main(String[] args) throws Exception{
     new Main().solution();
-  }
-  public boolean containsFalse(boolean[] check){
-    for(boolean b : check){
-      if(b== false) return true;
-    }
-    return false;
   }
 }
