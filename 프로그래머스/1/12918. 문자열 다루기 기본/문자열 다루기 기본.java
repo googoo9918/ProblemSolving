e744eea88defbed8009a61a64f81fd1958646e11
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-07-23 11:36:14 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(String s) {
+        if(s.length()!=4 && s.length()!=6) return false;
+        boolean answer = true;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) >= 65) return false;
+        }
+        return answer;
+    }
+}
