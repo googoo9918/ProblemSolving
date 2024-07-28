833fe9a0375fdf7f35ed23bd42168e923431df68
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-07-28 19:18:28 +0900

    [level 1] Title: 기사단원의 무기, Time: 125.29 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            int count = countDivisors(i);
+            answer += (count > limit) ? power : count;
+        }
+        return answer;
+    }
+
+    private int countDivisors(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count += (i * i == n) ? 1 : 2;
+            }
+        }
+        return count;
+    }
+}
