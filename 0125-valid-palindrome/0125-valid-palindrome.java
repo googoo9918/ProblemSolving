074ef51896d5e34ca726d9ea815033b59e98c0c1
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-14 20:10:23 +0900

    Time: 3 ms (72.91%), Space: 42.9 MB (76.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,17 +3,25 @@ import java.io.*;
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        s = s.replaceAll("[^a-z0-9]", "");
+        // s = s.replaceAll("[^a-z0-9]", "");
         // StringBuilder sb = new StringBuilder(s);
         // return s.equals(sb.reverse().toString());
         int left = 0, right = s.length()-1;
-        char[] carr = s.toCharArray();
+        // char[] carr = s.toCharArray();
         while(left<right){
-            char c = carr[left];
-            carr[left] = carr[right];
-            carr[right] = c;
-            left++; right--;
+            if(!Character.isLetterOrDigit(s.charAt(left))) left++;
+            else if(!Character.isLetterOrDigit(s.charAt(right))) right--;
+            else{
+                if(s.charAt(left) != s.charAt(right)) return false;
+                left++;
+                right--; 
+            }
+            // char c = carr[left];
+            // carr[left] = carr[right];
+            // carr[right] = c;
+            // left++; right--;
         }
-        return s.equals(new String(carr));
+        // return s.equals(new String(carr));
+        return true;
     }
 }
