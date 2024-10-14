787691ad2c9b9d4fc8724398de37706c773dce24
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-14 20:11:22 +0900

    Time: 2 ms (99.19%), Space: 43.2 MB (60.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
+        // s = s.toLowerCase();
         // s = s.replaceAll("[^a-z0-9]", "");
         // StringBuilder sb = new StringBuilder(s);
         // return s.equals(sb.reverse().toString());
@@ -12,7 +12,7 @@ class Solution {
             if(!Character.isLetterOrDigit(s.charAt(left))) left++;
             else if(!Character.isLetterOrDigit(s.charAt(right))) right--;
             else{
-                if(s.charAt(left) != s.charAt(right)) return false;
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
                 left++;
                 right--; 
             }
