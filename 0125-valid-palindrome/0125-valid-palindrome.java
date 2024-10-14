860d9e67f7298b3304acc287c93403a318c0bde3
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-14 19:58:23 +0900

    Time: 14 ms (34.47%), Space: 44.9 MB (25.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,7 +4,16 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
         s = s.replaceAll("[^a-z0-9]", "");
-        StringBuilder sb = new StringBuilder(s);
-        return s.equals(sb.reverse().toString());
+        // StringBuilder sb = new StringBuilder(s);
+        // return s.equals(sb.reverse().toString());
+        int left = 0, right = s.length()-1;
+        char[] carr = s.toCharArray();
+        while(left<right){
+            char c = carr[left];
+            carr[left] = carr[right];
+            carr[right] = c;
+            left++; right--;
+        }
+        return s.equals(new String(carr));
     }
 }
