5dcbeeffa4a49feb90f88af76896f84cddd22785
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-15 15:07:33 +0900

    Time: 6 ms (97.89%), Space: 47.8 MB (55.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        String[] tstrs = Arrays.copyOf(strs, strs.length);
-        for(int i=0; i<tstrs.length; i++){
-            char[] carr = tstrs[i].toCharArray();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] carr = str.toCharArray();
             Arrays.sort(carr);
-            tstrs[i] = new String(carr);
-            System.out.println(tstrs[i]);
-        }
 
-        Map<String, List<Integer>> map = new HashMap<>();
-
-        for(int i=0; i<tstrs.length; i++){
-            if(!map.containsKey(tstrs[i])){
-                map.put(tstrs[i], new ArrayList<>());
+            String s = String.valueOf(carr);
+            if(!map.containsKey(s)){
+                map.put(s, new ArrayList<>());
             }
-            map.get(tstrs[i]).add(i);
+            map.get(s).add(str);
         }
 
-        List<List<String>> list = new ArrayList<>();
-        for(Map.Entry<String, List<Integer>> entry : map.entrySet()){
-            List<Integer> ilist = entry.getValue();
-            List<String> slist = new ArrayList<>();
-            for(int i=0; i<ilist.size(); i++){
-                slist.add(strs[ilist.get(i)]);
-            }
-            list.add(slist);
-        }
-        return list;
+        return new ArrayList<>(map.values());
     }
 }
