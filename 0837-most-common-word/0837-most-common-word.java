6c36205f1a654b57faa143363f14814384a64b50
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-15 13:57:44 +0900

    Time: 14 ms (70.25%), Space: 42.6 MB (85.58%) - LeetHub

---
+++ 0837-most-common-word/0837-most-common-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase();
+        String[] pgList = paragraph.split("[ !?';,.]+");
+        List<String> list = new ArrayList<>(Arrays.asList(banned));
+        Map<String, Integer> map = new HashMap<>();
+        for(String pg : pgList){
+            if(!list.contains(pg)){
+                map.put(pg, map.getOrDefault(pg,0)+1);
+            }
+        }
+
+        String answer = "";
+        int max = 0;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            int tmp = entry.getValue();
+            if(tmp>max){
+                max = tmp;
+                answer = entry.getKey();
+            }
+        }
+
+        return answer;
+    }
+}
