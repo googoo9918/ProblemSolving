953e858678ba8b952570d1f97729213611925085
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-16 09:57:07 +0900

    Time: 1 ms (67.17%), Space: 46.5 MB (28.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int sum = 0, maxIdx = 0, max = 0;
-        for(int i=0; i<height.length; i++){
-            if(max<height[i]){
-                max = height[i];
-                maxIdx = i;
+        int sum =0, left=0, right = height.length-1;
+        int leftMax = height[left], rightMax = height[right];
+
+        while(left<right){
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+
+            if(leftMax<=rightMax){
+                sum += leftMax - height[left];
+                left++;
+            }
+            else{
+                sum += rightMax - height[right];
+                right--;
             }
         }
-        int leftMax = height[0];
-        for(int i=0; i<maxIdx; i++){
-            leftMax = Math.max(leftMax, height[i]);
-            sum += leftMax - height[i];
-        }
-
-        int rightMax = height[height.length-1];
-        for(int i=height.length-1; i>maxIdx; i--){
-            rightMax = Math.max(rightMax, height[i]);
-            sum += rightMax - height[i];
-        }
-
         return sum;
     }
 }
