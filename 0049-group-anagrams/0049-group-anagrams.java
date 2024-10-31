d873ac04ba72ad1ac767eafb5a39dbceaca9d54e
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-31 14:13:38 +0900

    Time: 7 ms (65.02%), Space: 47.7 MB (68.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,37 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<char[]> clist = new ArrayList<>();
-        for(String str : strs){
-            clist.add(str.toCharArray());
-        }
-
-        List<String> slist = new ArrayList<>();
-        
-        for(char[] carr : clist){
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] carr = s.toCharArray();
             Arrays.sort(carr);
-            slist.add(new String(carr));
-        }
+            String key = String.valueOf(carr);
 
-        for(String s : slist){
-            System.out.println(s);
-        }
-        List<List<String>> answer = new ArrayList<>();
-        Set<String> check = new HashSet<>();
-
-        for(int i=0; i<slist.size(); i++){
-            String stmp = slist.get(i);
-            if(!check.contains(stmp)){
-                List<String> tmp = new ArrayList<>();
-                for(int j=i; j<slist.size(); j++){
-                    if(stmp.equals(slist.get(j))){
-                        tmp.add(strs[j]);
-                    }
-                }
-                answer.add(tmp);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
             }
-            check.add(stmp);
+            map.get(key).add(s);
         }
 
-        return answer;
+        return new ArrayList<>(map.values());
     }
 }
