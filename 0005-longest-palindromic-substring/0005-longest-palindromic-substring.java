ce433567401aac36487c8c3b77ddd5afd0f1cccd
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-10-31 17:51:20 +0900

    Time: 14 ms (93.08%), Space: 42.4 MB (71.56%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,24 @@
 class Solution {
+    int left,right;
     public String longestPalindrome(String s) {
-        if(s.length()<2) return s;
-        String ans = String.valueOf(s.charAt(0));
-        for(int mid=1; mid<s.length()-1; mid++){
-            int left = mid-1, right = mid+1;
-            while(left>=0 && right<=s.length()-1){
-                if(s.charAt(left)==s.charAt(right)){
-                    if(ans.length()<right-left+1){
-                        ans = s.substring(left, right+1);
-                    }
-                    left--; right++;
-                }else{
-                    break;
-                }
-            }
-        }
+        if(s.length()<1) return s;
 
-        for(int mid=0; mid<s.length()-1; mid++){
-            int left =mid, right = mid+1;
-            while(left>=0 && right<=s.length()-1){
-                if(s.charAt(left)==s.charAt(right)){
-                    if(ans.length()<right-left+1){
-                        ans = s.substring(left, right+1);
-                    }
-                    left--; right++;
-                }else{
-                    break;
-                }
-            }
+        for(int i=0; i<s.length(); i++){
+            findLongPal(s, i, i+1);
+            findLongPal(s, i, i+2);
         }
-        return ans;
+        return s.substring(left, right+1);
+    }
+
+    public void findLongPal(String s, int start, int end){
+        while(start>=0 && end<=s.length()-1 && s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
+        }
+        start++; end--;
+        if(end-start+1 > right-left+1){
+            left = start;
+            right = end;
+        }
     }
 }
