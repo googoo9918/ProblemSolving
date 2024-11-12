24c59a50ec12cdfcfb71d7dd5f64f6b99b299364
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-11-12 18:10:56 +0900

    Time: 15 ms (93.16%), Space: 94.5 MB (46.15%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,25 +1,21 @@
 class Solution {
-    private void dfs(List<List<Integer>> answer, List<Integer> ans, boolean[] used, int n, int k,int idx){
-        if(ans.size() == k){
+    private void dfs(List<List<Integer>> answer, List<Integer> ans, int n, int k, int idx) {
+        if (ans.size() == k) {
             answer.add(new ArrayList<>(ans));
             return;
         }
 
-        for(int i=idx; i<=n; i++){
-            if(used[i-1]) continue;
+        for (int i = idx; i <= n - (k - ans.size()) + 1; i++) {
             ans.add(i);
-            used[i-1] = true;
-            dfs(answer, ans, used, n, k, i+1);
-            ans.remove(ans.size()-1);
-            used[i-1] = false;
+            dfs(answer, ans, n, k, i + 1);
+            ans.remove(ans.size() - 1);
         }
     }
+
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        boolean[] used = new boolean[n];
-
-        dfs(answer, ans, used, n, k, 1);
+        dfs(answer, ans, n, k, 1);
         return answer;
     }
 }
