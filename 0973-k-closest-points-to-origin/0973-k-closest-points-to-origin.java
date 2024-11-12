7b1e4710f13dad372bc4215e1b02eb02c06fd91d
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-11-12 11:33:19 +0900

    Time: 24 ms (75.85%), Space: 53.2 MB (46.15%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,30 +1,26 @@
 class Solution {
-    class Point implements Comparable<Point>{
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
-            this.distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)); 
-        }
-        int x;
-        int y;
-        double distance;
-
-        @Override
-        public int compareTo(Point other){
-            return Double.compare(this.distance, other.distance);
+    class Point{
+        int[] point;
+        long distance;
+        Point(int[] point, long distance){
+            this.point = point;
+            this.distance = distance;
         }
     }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-        for(int[] iarr : points){
-            Point p = new Point(iarr[0], iarr[1]);
+        PriorityQueue<Point> pq = new PriorityQueue<>((p1,p2) ->{
+            return Long.compare(p1.distance,p2.distance);
+        });
+        
+        for(int[] point : points){
+            long distance = point[0]*point[0] + point[1]*point[1];
+            Point p = new Point(point, distance);
             pq.add(p);
         }
 
         int[][] ans = new int[k][2];
         for(int i=0; i<k; i++){
-            ans[i][0] = pq.peek().x;
-            ans[i][1] = pq.poll().y;
+            ans[i] = pq.poll().point;
         }
 
         return ans;
