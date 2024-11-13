39855a0e6514a2baa830e9997dac0462245b2454
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2024-11-13 17:58:11 +0900

    Time: 1 ms (56.72%), Space: 41.8 MB (88.52%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,29 @@
 class Solution {
-    public void dfs(List<String> answer, Map<Character, String> numbers, String digits, int index, StringBuilder sb){
-        if(digits.length() == sb.length()){
-            answer.add(sb.toString());
+    private void dfs(List<String> ans, Map<Character,String> map, String digits, int start, StringBuilder sb){
+        if(sb.length() == digits.length()){
+            ans.add(sb.toString());
             return;
         }
 
-        for(char c : numbers.get(digits.charAt(index)).toCharArray()){
-            dfs(answer, numbers, digits, index+1, new StringBuilder(sb).append(c));
+        for(char c : map.get(digits.charAt(start)).toCharArray()){
+            dfs(ans, map, digits, start+1, new StringBuilder(sb).append(c));
         }
     }
     public List<String> letterCombinations(String digits) {
-        List<String> answer = new ArrayList<>();
-        if(digits.length() == 0) return answer;
-        Map<Character, String> numbers = new HashMap<>();
-        numbers.put('2', "abc");
-        numbers.put('3', "def");
-        numbers.put('4', "ghi");
-        numbers.put('5', "jkl");
-        numbers.put('6', "mno");
-        numbers.put('7', "pqrs");
-        numbers.put('8', "tuv");
-        numbers.put('9', "wxyz");
+        List<String> ans = new ArrayList<>();
+        if(digits.length()==0) return ans;
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
 
-        dfs(answer, numbers, digits, 0, new StringBuilder());
-        return answer;
+        dfs(ans, map, digits, 0, new StringBuilder());
+
+        return ans;
     }
 }
