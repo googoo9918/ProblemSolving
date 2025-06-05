92d4e59bcb53c1ab36084b05730337ef2e3db4b9
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-06-05 10:12:21 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 23.11 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(String term : terms){
+            String[] termArr = term.split(" ");
+            map.put(termArr[0].charAt(0), Integer.parseInt(termArr[1]));
+        }
+        
+        int idx = 1;
+        List<Integer> list = new ArrayList<>();
+        for(String privacy : privacies){
+            int afterPrivacy = StringToSum(privacy);
+            int plus = map.get(privacy.split(" ")[1].charAt(0));
+            if(afterPrivacy + 28*plus -1 < StringToSum(today)){
+                list.add(idx);
+            }
+            System.out.println("idx: " + idx + " afterPrivacy: " + (afterPrivacy + 28*plus) + " today: " + StringToSum(today));
+            idx++;
+        }
+        
+        int[] ans = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            ans[i] = list.get(i);
+        }
+        
+        return ans;
+    }
+    
+    private int StringToSum(String privacy){
+        String[] privacyArr = privacy.split(" ");
+        String[] privacyDate = privacyArr[0].split("\\.");
+        
+        int year = Integer.parseInt(privacyDate[0]);
+        int month = Integer.parseInt(privacyDate[1]);
+        int day = Integer.parseInt(privacyDate[2]);
+        
+        int sum = day + 28 * (12 * year + month);
+        
+        return sum;
+    }
+}
