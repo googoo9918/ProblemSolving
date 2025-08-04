0cf55b4861e0d584c25da172ad1e9f6fca3eff58
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-08-04 19:59:58 +0900

    Time: 14 ms (57.56%), Space: 49 MB (25.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,40 @@
+import java.util.*;
 class Solution {
-    class Num{
-        int num;
-        int cnt;
-        Num(int num, int cnt){
-            this.num = num;
-            this.cnt = cnt;
+    class Element{
+        int key;
+        int nums;
+
+        Element(int key, int nums){
+            this.key = key;
+            this.nums = nums;
         }
     }
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Num> pq = new PriorityQueue<>((n1, n2) -> {
-            return Integer.compare(n2.cnt,n1.cnt);
-        });
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i,0)+1);
         }
+
+        PriorityQueue<Element> pq = new PriorityQueue<>(new Comparator<Element>(){
+            @Override
+            public int compare(Element e1, Element e2){
+                return e2.nums - e1.nums;
+            }
+        });
 
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            Num n = new Num(entry.getKey(), entry.getValue());
-            pq.add(n);
+            Element e1 = new Element(entry.getKey(), entry.getValue());
+
+            pq.add(e1);
         }
 
-        int[] ans = new int[k];
+        int[] iarr = new int[k];
+
         for(int i=0; i<k; i++){
-            ans[i] = pq.poll().num;
+            iarr[i] = pq.poll().key;
         }
-        return ans;
+
+        return iarr;
     }
 }
