19e73e618cdd1fd40d46c8bd072650ae46531768
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-08-05 20:45:13 +0900

    Time: 1 ms (49.3%), Space: 42.7 MB (82.64%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,19 @@
+import java.util.*;
 class Solution {
-    private void dfs(List<List<Integer>> answer, List<Integer> ans, int[] nums, int start, int size){
-        if(ans.size()==size){
-            answer.add(new ArrayList<>(ans));
-            return;
-        }
-
+    private void dfs(List<List<Integer>> ans, List<Integer> tmp, int[] nums, int start){
+        ans.add(new ArrayList<>(tmp));
         for(int i=start; i<nums.length; i++){
-            ans.add(nums[i]);
-            dfs(answer, ans, nums, i+1, size);
-            ans.remove(ans.size()-1);
+            tmp.add(nums[i]);
+            dfs(ans, tmp, nums, i+1);
+            tmp.remove(tmp.size()-1);
         }
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
 
-        for(int i=0; i<=nums.length; i++){
-            dfs(answer, ans, nums, 0, i);
-        }
+        dfs(ans, tmp, nums, 0);
 
-        return answer;
+        return ans;
     }
 }
