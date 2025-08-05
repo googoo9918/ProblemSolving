5cd34062e819d17ba2b93fe932381c4e733ed517
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-08-05 20:08:12 +0900

    Time: 18 ms (79.28%), Space: 95 MB (17.45%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,21 +1,33 @@
+import java.util.*;
 class Solution {
-    private void dfs(List<List<Integer>> answer, List<Integer> ans, int n, int k, int idx) {
-        if (ans.size() == k) {
-            answer.add(new ArrayList<>(ans));
+    private void dfs(int n, int k, List<List<Integer>> ans, List<Integer> tmp, int[] nums, boolean[] check, int start){
+        if(tmp.size() == k){
+            ans.add(new ArrayList<>(tmp));
             return;
         }
 
-        for (int i = idx; i <= n - (k - ans.size()) + 1; i++) {
-            ans.add(i);
-            dfs(answer, ans, n, k, i + 1);
-            ans.remove(ans.size() - 1);
+        for(int i=start; i<n; i++){
+            if(check[i]) continue;
+
+            tmp.add(nums[i]);
+            check[i] = true;
+            dfs(n, k, ans, tmp, nums, check, i+1);
+            check[i] = false;
+            tmp.remove(tmp.size()-1);
         }
     }
-
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        dfs(answer, ans, n, k, 1);
-        return answer;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        boolean[] check = new boolean[n];
+        int[] nums = new int[n];
+
+        for(int i=0; i<n; i++){
+            nums[i] = i+1;
+        }
+
+        dfs(n, k, ans, tmp, nums, check, 0);
+
+        return ans;
     }
 }
