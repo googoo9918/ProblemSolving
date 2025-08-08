b2e0c03dd2fdd92fc5480e67dca420f0548ae582
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-08-08 19:24:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        Set<String> set = new HashSet<>();
+        int cnt = 0, turn = 0;
+        set.add(words[0]);
+        for(int i=1; i<words.length; i++){
+            if(words[i].charAt(0) != words[i-1].charAt(words[i-1].length()-1) || set.contains(words[i])){
+                cnt = (i%n) + 1;
+                turn = (i/n) + 1;
+                break;
+            }
+            set.add(words[i]);
+        }
+        
+        return new int[]{cnt, turn};
+    }
+}
