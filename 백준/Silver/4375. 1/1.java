95d34241fe0e27b1577337f508b0f9187795274d
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-11-19 13:14:17 +0900

    [Silver III] Title: 1, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = br.readLine()) != null) {
+            int n = Integer.parseInt(line);
+
+            int mod = 1%n;
+            int cnt = 1;
+            while(mod != 0){
+                mod = (mod * 10 + 1) % n;
+                cnt++;
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
