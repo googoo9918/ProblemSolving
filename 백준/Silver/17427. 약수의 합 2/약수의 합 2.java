a7c96e02c26640f25138037976d5855b1616f9e7
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-11-19 13:44:30 +0900

    [Silver II] Title: 약수의 합 2, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+
+        for(int i=1; i<=n; i++){
+            sum += (long) i * (n/i);
+        }
+
+        System.out.println(sum);
+    }
+}
