7d45bdf4d5a5a3431089e5f54468c1400c20c010
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-11-24 20:29:42 +0900

    [Silver V] Title: 셀프 넘버, Time: 96 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,36 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        boolean[] barr = new boolean[10001];
+        boolean[] barr = new boolean[10001]; // 0~10000, 0은 사용 안 함
 
-        for(int i=1; i<=10000; i++){
-            int tmp = i;
-            tmp = construct(tmp);
-            while(tmp <= 10000){
-                if(!barr[tmp]){
-                    barr[tmp] = true;
-                }
-                tmp = construct(tmp);
+        // 1. 1부터 10000까지의 i에 대해 d(i)를 한 번씩만 계산
+        for (int i = 1; i <= 10000; i++) {
+            int n = construct(i);   // d(i)
+            if (n <= 10000) {       // 범위 안에만 표시
+                barr[n] = true;     // n은 "생성된 수"이므로 셀프 넘버가 아님
             }
         }
 
+        // 2. 한 번도 생성되지 않은 수(= 셀프 넘버)만 출력
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=10000; i++){
-            if(!barr[i]){
-                sb.append(i);
-                sb.append("\n");
+        for (int i = 1; i <= 10000; i++) {
+            if (!barr[i]) {
+                sb.append(i).append('\n');
             }
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    public static int construct(int tmp){
-        int ans = tmp;
-        while(tmp > 0){
-            ans += tmp%10;
-            tmp /= 10;
+    // d(n) = n + 각 자리수의 합
+    public static int construct(int n) {
+        int ans = n;
+        while (n > 0) {
+            ans += n % 10;
+            n /= 10;
         }
-
         return ans;
     }
 }
