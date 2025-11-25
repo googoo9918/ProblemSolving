f9389c6fd104b8d8718f5049f715ac49b1de7b53
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-11-25 19:13:00 +0900

    [Silver IV] Title: 토너먼트, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -3,35 +3,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, K, L;
-  public void solution() throws Exception{
-    // 16 8+4+2+1
-    // 1000 500+250+125+64+32+16+8+4+2+1
-    // 12 / 34 / 56 / 78
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    K = Integer.parseInt(st.nextToken());
-    L = Integer.parseInt(st.nextToken());
-    int round = 1;
-    int cnt =0;
-    while(N>1){
-      N/=2;
-      cnt++;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int cnt = 1;
+        for(int i=0; i<=n/2; i++){
+            int ktmp = (k+1)/2;
+            int ltmp = (l+1)/2;
+            if(ktmp == ltmp){
+                System.out.println(cnt);
+                break;
+            }
+
+            k = ktmp;
+            l = ltmp;
+            cnt++;
+        }
     }
-    for(int i=0; i<cnt; i++){
-      if(K%2==1) K++;
-      if(L%2==1) L++;
-      if(K/2 == L/2) break;
-      else{
-        K/=2;
-        L/=2;
-        round++;
-      }
-    }
-    System.out.println(round);
-  }
-  public static void main(String[] args) throws Exception{
-    new Main().solution();
-  }
 }
