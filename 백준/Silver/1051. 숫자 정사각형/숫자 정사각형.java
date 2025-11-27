5f7189ac18264eef21de08c92b157ac2d6295a97
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-11-27 19:45:22 +0900

    [Silver III] Title: 숫자 정사각형, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -3,47 +3,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, M;
-  static int[][] rectangle;
-  public void solution() throws Exception{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    rectangle = new int[N][M];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-    for(int i=0; i<N; i++){
-      String s = br.readLine();
-      for(int j=0; j<M; j++){
-        rectangle[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
-      }
-    }
-    int min = Math.min(N,M)-1;
-    System.out.println(calculateSize(min));
-  }
+        String[] sarr = new String[n];
 
-  private int calculateSize(int min) {
-    for(int i=min; i>=1; i--){
-      for(int j=0; j<N-i; j++){
-        for(int k=0; k<M-i; k++){
-          if(isSquare(i, j, k)) {
-            return (i+1)*(i+1);
-          }
+        for(int i=0; i<n; i++){
+            sarr[i] = br.readLine();
         }
-      }
+
+        int min = Math.min(n, m);
+        int ans = 1;
+        for(int i=1; i<min; i++){
+            for(int j=0; j<=n-i-1; j++){
+                for(int k=0; k<=m-i-1; k++){
+                    if(sarr[j].charAt(k) == sarr[j].charAt(k+i) && sarr[j].charAt(k) == sarr[j+i].charAt(k) && sarr[j].charAt(k)== sarr[j+i].charAt(k+i)){
+                        ans = (i+1) * (i+1);
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
-    return 1;
-  }
-
-  private boolean isSquare(int i, int j, int k) {
-    if(rectangle[j][k] != rectangle[j+i][k]) return false;
-    if(rectangle[j][k] != rectangle[j][k+i]) return false;
-    if(rectangle[j][k] != rectangle[j+i][k+i]) return false;
-    return true;
-  }
-
-  public static void main(String[] args) throws Exception{
-    new Main().solution();
-  }
-
 }
