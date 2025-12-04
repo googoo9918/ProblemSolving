f1e12b7395bd80dc5b46b6a41fc2222c44c3e4dd
googoo9918 <102513932+googoo9918@users.noreply.github.com>
2025-12-04 20:07:53 +0900

    [Silver III] Title: N과 M (8), Time: 168 ms, Memory: 20016 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] iarr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            iarr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(iarr);
+
+        List<Integer> list = new ArrayList<>();
+        dfs(n, m, iarr, list, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m, int[] iarr, List<Integer> list, int start) {
+        if (list.size() == m) {
+            for (int i : list) {
+                sb.append(i).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            list.add(iarr[i]);
+            dfs(n, m, iarr, list, i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
